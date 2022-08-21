9ca67ac695da8caebc8312e968e61f0f0fe3dc71
Santosh Kumar <quietudestudio@gmail.com>
2022-08-21 15:39:16 +0530

    finally

---
+++ server/Routes/UserRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { deleteUser, followUser, getAllUsers, getUser, UnfollowUser, updateUser } from "../Controller/UserController.js";
+const router = express.Router(); 
+
+router.get('/', getAllUsers)
+router.get('/:id', getUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
+router.put('/:id/follow', followUser)
+router.put('/:id/unfollow', UnfollowUser)
+
+export default router;
